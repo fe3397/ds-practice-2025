d197286ae7e4d9bb5bd7be738adbd5e7e0af0c91
fe3397 <felix.aschpurwis@ut.ee>
2025-03-05 11:28:52 +0200

    Added suggestion server with some logic plus modified docker and docker compose

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -5,21 +5,22 @@ import os
 # The path of the stubs is relative to the current file, or absolute inside the container.
 # Change these lines only if strictly needed.
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
-fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/fraud_detection'))
+fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/suggestion_service'))
 sys.path.insert(0, fraud_detection_grpc_path)
-import fraud_detection_pb2 as fraud_detection
-import fraud_detection_pb2_grpc as fraud_detection_grpc
+import suggestion_service_pb2 as suggestion_service
+import suggestion_service_pb2_grpc as suggestion_service_grpc
 
 import grpc
 
-def greet(name='you'):
+def suggest(book1, book2):
     # Establish a connection with the fraud-detection gRPC service.
-    with grpc.insecure_channel('fraud_detection:50051') as channel:
+    with grpc.insecure_channel('suggestion_service:50053') as channel:
         # Create a stub object.
-        stub = fraud_detection_grpc.HelloServiceStub(channel)
+        stub = suggestion_service_grpc.SuggestionStub(channel)
         # Call the service through the stub object.
-        response = stub.SayHello(fraud_detection.HelloRequest(name=name))
-    return response.greeting
+        response = stub.MakeSuggestion(suggestion_service.SuggestionRequest(book_1=book1, book_2=book2))
+        # Return the response.
+    return response
 
 # Import Flask.
 # Flask is a web framework for Python.
@@ -54,14 +55,15 @@ def checkout():
     request_data = json.loads(request.data)
     # Print request object data
     print("Request Data:", request_data.get('items'))
+    suggestions = suggest(request_data.get('items')[0]['name'], request_data.get('items')[1]['name'])
 
     # Dummy response following the provided YAML specification for the bookstore
     order_status_response = {
         'orderId': '12345',
         'status': 'Order Approved',
         'suggestedBooks': [
-            {'bookId': '123', 'title': 'The Best Book', 'author': 'Author 1'},
-            {'bookId': '456', 'title': 'The Second Best Book', 'author': 'Author 2'}
+            {'bookId': '123', 'title': suggestions.sug_book_1, 'author': 'Author 1'},
+            {'bookId': '456', 'title': suggestions.sug_book_2, 'author': 'Author 2'}
         ]
     }
 
